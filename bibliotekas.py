ff70c23f6252b68f6c9828dce672245b131b26fc
henrietei <113431841+henrietei@users.noreply.github.com>
2022-10-13 15:09:27 +0300

    lab

---
+++ bibliotekas.py
@@ -0,0 +1,21 @@
+'''from termcolor2 import c
+
+print(c("Some text").white.on_blue.underline)
+print("Datorika")
+print(c("Jauna rindkopa").red)
+print()
+
+import wikipedia
+wikipedia.set_lang("lv")
+print(wikipedia.summary('Riga'))
+print()'''
+
+import pyperclip
+pyperclip.copy("dfgnjng")
+pyperclip.paste()
+
+import emojis
+emojified = emojis.encode(":snake: :boot: :yellow_heart:  :innocent:!")
+print(emojified)
+  
+
