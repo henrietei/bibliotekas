1bf433c6f47b0c4dd167d0351d2505775f16b28e
henrietei <113431841+henrietei@users.noreply.github.com>
2022-10-13 15:44:02 +0300

    labb

--- bibliotekas.py
+++ bibliotekas.py
@@ -1,4 +1,4 @@
-'''from termcolor2 import c
+"""from termcolor2 import c
 
 print(c("Some text").white.on_blue.underline)
 print("Datorika")
@@ -8,14 +8,55 @@ print()
 import wikipedia
 wikipedia.set_lang("lv")
 print(wikipedia.summary('Riga'))
-print()'''
+print()
 
 import pyperclip
-pyperclip.copy("dfgnjng")
+pyperclip.copy("ifgjdfgkdfngfjkg")
 pyperclip.paste()
 
 import emojis
 emojified = emojis.encode(":snake: :boot: :yellow_heart:  :innocent:!")
 print(emojified)
   
+from PIL import Image
+
+def bilde():
+    datne="bilde.jpg"
+    with Image.open(datne) as im:
+        print(datne, im.format, f"{im.size}x{im.mode}")
+        im.show()
+        izmers=(100,100)
+        im.thumbnail(izmers)
+        im.save("bilde-maza.jpg",im.format)
+        im.show()
+        
+bilde()"""
+
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def apstrada_lapu(url):
+    r=requests.get(url)
+    html=r.textsoup=BeautifulSoup(html, 'html.parser')
+    return soup
+
+html=apstrada_lapu("https://www.tvnet.lv")
+print(html.head.title.text)
+
+"""import matplotlib.pyplot as plt
+import numpy as np
+
+def linijas():
+    ypoints=np.array(np.random.randint(100, size=(10)))
+    ypoints2=np.array(np.random.randint(100, size=(10)))
+    ypoints3=np.array(np.random.randint(100, size=(60)))
+
+    plt.plot(ypoints, 'o-')
+    plt.plot(ypoints2, '+:r')
+    plt.plot(ypoints3, '')
+    plt.show()
+
+linijas()"""
 
